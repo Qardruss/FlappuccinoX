588d974b7520acb24485612a3af69dde92e6943f
thatretrodev <thatretrodev@outlook.com>
2021-08-10 15:56:36 -0400

    Fix syntax and add more comments

--- bean.py
+++ bean.py
@@ -1,6 +1,10 @@
 import pygame
+
 class Bean: 
-    def __init__(self):
-        self.sprite = pygame.image.load('data/gfx/bean.png')
-        self.position = pygame.Vector2()
-        self.position.xy
+	def __init__(self):
+		# Load the bean sprite
+		self.sprite = pygame.image.load('data/gfx/bean.png')
+
+		# Handle the position of the bean
+		self.position = pygame.Vector2()
+		self.position.xy
--- player.py
+++ player.py
@@ -1,11 +1,18 @@
 import pygame
 
 class Player:
-    position = pygame.Vector2()
-    position.xy = 295, 100
-    velocity = pygame.Vector2()
-    velocity.xy = 3, 0
-    acceleration = 0.1
-    rightSprite = pygame.image.load('data/gfx/player.png')
-    leftSprite = pygame.transform.flip(rightSprite, True, False)
-    currentSprite = rightSprite
+	# Player position
+	position = pygame.Vector2()
+	position.xy = 295, 100
+
+	# Player velocity & Acceleration
+	velocity = pygame.Vector2()
+	velocity.xy = 3, 0
+	acceleration = 0.1
+
+	# Player sprites
+	rightSprite = pygame.image.load('data/gfx/player.png')
+	leftSprite = pygame.transform.flip(rightSprite, True, False)
+
+	# The current Player sprite
+	currentSprite = rightSprite
